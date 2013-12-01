e3da7a3953e7074d479b553dc0de637d8a13b755
Todd Wolfson <todd@twolfson.com>
2013-11-30 21:00:30 -0800

    Automating testing of remaining files

--- test/center-text_test.js
+++ test/center-text_test.js
@@ -1,15 +1,19 @@
 var assert = require('assert');
 var centerText = require('../');
 
+function runCenterText() {
+  before(function () {
+    this.actual = centerText(this.file.input, {columns: this.file.columns});
+  });
+}
+
 describe('An even string in an even amount of columns', function () {
   before(function () {
     this.file = require('./test-files/even-in-even');
   });
 
   describe('when centered', function () {
-    before(function () {
-      this.actual = centerText(this.file.input, {columns: this.file.columns});
-    });
+    runCenterText();
 
     it('generates centered text', function () {
       assert.strictEqual(this.file.expected, this.actual);
@@ -23,11 +27,29 @@ describe('An ANSI colored string', function () {
   });
 
   describe('when centered', function () {
-    before(function () {
-      this.actual = centerText(this.file.input, {columns: this.file.columns});
-    });
+    runCenterText();
 
     it('generates colored centered text', function () {
+      assert.strictEqual(this.file.expected, this.actual);
+    });
+  });
+});
+
+// Automate the remaining test cases
+var glob = require('glob');
+var remainingFiles = glob
+                       .sync('*.js', {cwd: __dirname + '/test-files'})
+                       .filter(function (filename) {
+                         return !filename.match(/ansi-colors|even-in-even/);
+                       });
+remainingFiles.forEach(function (filename) {
+  describe(filename, function () {
+    before(function () {
+      this.file = require(__dirname + '/test-files/' + filename);
+    });
+    runCenterText();
+
+    it('generates centered text', function () {
       assert.strictEqual(this.file.expected, this.actual);
     });
   });
